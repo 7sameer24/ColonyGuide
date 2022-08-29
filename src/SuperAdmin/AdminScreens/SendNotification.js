f879780f2b33d16c9e50aa7f8d44aff1c75f8e11
Sameer Hussain <7Sameer24@gmail.com>
2022-08-29 18:30:03 +0530

    Update

--- src/SuperAdmin/AdminScreens/SendNotification.js
+++ src/SuperAdmin/AdminScreens/SendNotification.js
@@ -11,6 +11,7 @@ import {useToast} from 'react-native-toast-notifications';
 import NoDataAni from '../../Components/NoDataAni';
 import Spinner from '../../Components/Spinner';
 import ButtonComponent from '../../Components/ButtonComponent';
+import moment from 'moment';
 
 const SendNotification = () => {
   const toast = useToast();
@@ -45,6 +46,16 @@ const SendNotification = () => {
       updateData([]);
     };
   }, []);
+
+  const timeFormatter = cell => {
+    if (cell !== null || cell !== undefined) {
+      let correctDate = moment(new Date(cell)).format('DD-MMM-YYYY hh:mm');
+      console.log(correctDate);
+      return correctDate;
+    } else {
+      return 'NA';
+    }
+  };
   return (
     <View style={genericStyles.Container}>
       <InputComponent
@@ -67,11 +78,13 @@ const SendNotification = () => {
                     longText={2}
                     title={item.message}
                     source={{uri: item.image}}
+                    AddressLine={timeFormatter(item.created_at)}
                     subTitle={`Notification sent : ${
                       item.notification_sent_count
                         ? item.notification_sent_count
                         : 0
                     }`}
+                    twoMore={true}
                   />
                 );
               })}
